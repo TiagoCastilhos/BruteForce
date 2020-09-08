e2152ccf21345470049ffcf0e333e046c2a3fef8
TiagoCastilhos <tiagocastilhos66@gmail.com>
2020-09-08 20:24:21 -0300

    numero de threads parametrizado

--- BruteForce/BruteForce.cpp
+++ BruteForce/BruteForce.cpp
@@ -6,19 +6,22 @@
 #else if
 #define omp_get_thread_num() 0
 #endif
-#define NUMTHREADS 8
 using namespace std;
 
 char chars[] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 string t;
 double startTime, endTime = 0;
+int numThreads = 1;
 
 void checkPassword(string password);
 void recurse(int width, int position, string baseString);
 
 int main() {
-	cout << "Enter a string: " << endl;
+	cout << "Digite uma senha: " << endl;
 	cin >> t;
+	cout << "Digite a quantidade de threads: " << endl;
+	cin >> numThreads;
+
 	int maxChars = 13;
 
 	startTime = omp_get_wtime();
@@ -31,7 +34,7 @@ int main() {
 }
 
 void recurse(int width, int position, string baseString) {
-#pragma omp parallel num_threads(NUMTHREADS)
+#pragma omp parallel num_threads(numThreads)
 #pragma omp for
 	for (int i = 0; i < 35; i++) {
 		if (position < width - 1) {
